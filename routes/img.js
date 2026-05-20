4d6019ace1395eeafe23213ab175f6d2b9361b36
EnyaLin <enyaliniris@gmail.com>
2026-05-20 16:20:16 +0800

    modify:intergrate router

--- routes/img.js
+++ routes/img.js
@@ -12,6 +12,13 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
+if (!process.env.CLOUDINARY_CLOUD_NAME || 
+    !process.env.CLOUDINARY_API_KEY || 
+    !process.env.CLOUDINARY_API_SECRET) {
+  console.error('缺少 Cloudinary 環境變數')
+  process.exit(1)
+}
+
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
@@ -26,4 +33,44 @@ router.post("/upload", upload.single("file"), async (req, res) => {
   res.json(req.file.path)  // path 是 Cloudinary 回傳的完整 URL
 })
 
+
+
+router.post("/updateimg", upload.single("file"), async (req, res) => {
+  if (res.locals.bearer.sid && res.locals.bearer.account) {
+    const sid = res.locals.bearer.sid
+    const img = req.file.path 
+
+    const sql_memberimg = `UPDATE members SET member_img = ? WHERE sid = ?`
+    await db.query(sql_memberimg, [img, sid])
+
+    const [r_sql_member] = await db.query(`SELECT * FROM members WHERE sid=?`, [sid])
+    res.json(r_sql_member[0] || {})
+  }
+})
+
+router.post("/myImg", async (req, res) => {
+  if (!res.locals.bearer) return res.status(401).json({ error: "未授權" });
+  
+  const myAuth = req.body.myAuth;
+  
+  if (!myAuth || !myAuth.accountId) {
+    return res.status(400).json({ error: "缺少 accountId" });
+  }
+
+  try {
+    const sql_myimg = `SELECT member_img FROM members WHERE sid=?`;
+    const [r_sql_myimg] = await db.query(sql_myimg, [myAuth.accountId]);
+    
+    const result = r_sql_myimg[0] || {}
+    console.log("img", result);
+    res.json(result);
+    
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "伺服器錯誤" })
+  }
+});
+
+
 module.exports = router;
+
